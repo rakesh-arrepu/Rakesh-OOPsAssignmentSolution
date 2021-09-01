07a8b31b2214486d74156978cfeb0ae8cd2e6566
rakesh-arrepu <38352324+rakesh-arrepu@users.noreply.github.com>
2021-09-01 12:38:46 +0530

    OOPs Assignment Solution

---
+++ src/com/gl/driver/Driver.java
@@ -0,0 +1,32 @@
+package com.gl.driver;
+
+import com.gl.departments.AdminDepartment;
+import com.gl.departments.HrDepartment;
+import com.gl.departments.TechDepartment;
+
+public class Driver{
+
+	public static void main(String[] args) {
+		AdminDepartment objAdmin = new AdminDepartment();		
+		System.out.println("Welcome to "+objAdmin.departmentName());
+		System.out.println(objAdmin.getTodaysWork());
+		System.out.println(objAdmin.getWorkDeadline());
+		System.out.println(objAdmin.isTodayAHoliday()+"\n");
+		
+		HrDepartment objHr = new HrDepartment();
+		System.out.println("Welcome to "+objHr.departmentName());
+		System.out.println(objHr.doActivity());
+		System.out.println(objHr.getTodaysWork());
+		System.out.println(objHr.getWorkDeadline());
+		System.out.println(objHr.isTodayAHoliday()+"\n");
+		System.out.println("");
+		
+		TechDepartment objTech = new TechDepartment();
+		System.out.println("Welcome to "+objTech.departmentName());
+		System.out.println(objTech.getTodaysWork());
+		System.out.println(objTech.getWorkDeadline());
+		System.out.println(objTech.getTechStackInformation());
+		System.out.println(objTech.isTodayAHoliday());
+	}
+
+}
